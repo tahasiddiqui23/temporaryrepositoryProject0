7065852c73ede75995726dcd3b48e32e7b62e327
tahasiddiqui23 <70670062+tahasiddiqui23@users.noreply.github.com>
2021-01-20 22:41:29 -0500

    submission

---
+++ Project 0/src/project/pkg0/Project0.java
@@ -0,0 +1,20 @@
+
+package project.pkg0;
+
+import java.util.Scanner;
+
+
+public class Project0 {
+    
+
+  
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner (System.in);
+        System.out.println("Hello young fellow what is your name?");
+        String name= keyboard.nextLine();
+        System.out.println("Hello whoever you are, my name is " +name+ " nice to meet you?");
+        keyboard.nextLine();
+        System.out.println("Ok cool I don't care have a good day man");
+    }
+    
+}
